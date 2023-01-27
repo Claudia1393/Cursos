e7da39294f92c15cd7338a3198e7288a07ae0d0b
Claudia1393 <118278662+Claudia1393@users.noreply.github.com>
2023-01-27 11:33:15 +0100

    Add files via upload

---
+++ Ejercicio_lista_completa.js
@@ -0,0 +1,18 @@
+
+const nombre="Claudia";
+const edad=29;
+const desarrollador =false;
+
+const fecha_nacimiento =new Date(1993,8,13);
+
+const libro ={
+    titulo: "El clan del oso cavernario",
+    autor: "Jean M Auel",
+    fecha: new Date(1980,0,15),
+    url:"https://es.wikipedia.org/wiki/El_clan_del_oso_cavernario"
+}
+
+const lista1 = [nombre,edad,desarrollador,fecha_nacimiento,libro];
+
+console.log(lista1)
+
