b856555e0d01994255d70721a71246dc7dbcd936
Claudia1393 <118278662+Claudia1393@users.noreply.github.com>
2023-02-03 10:28:48 +0100

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,10 @@
+// Ejercicio set
+const familia = ["Madre", "Padre", "Hermano", "Claudia"]
+
+const setFamilia = new Set(familia)
+
+setFamilia.add("Claudia")
+console.log(setFamilia)
+
+setFamilia.add("JavaScript")
+console.log(setFamilia)
